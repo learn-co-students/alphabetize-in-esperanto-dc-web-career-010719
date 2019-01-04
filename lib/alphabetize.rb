458e66c9197be5833b3d07226a769f76beec641e
Ross Jones <rossmosis.jones@gmail.com>
2019-01-03 23:16:14 -0500

    added method that sorts a given alphabet using sort_by

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+ESPERANTO_ALPHABET = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+
 def alphabetize(arr)
   # code here
+  arr.sort_by do |word|
+    word.split("").collect do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
